106bb0bcb14eb31b1693f02ae2aae1b5ea02ae3e
mpvader <mvader@victronenergy.com>
2015-02-02 15:10:46 +0000

    bumb version to v1.03

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -21,7 +21,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareVersion = '1.02'
+softwareVersion = '1.03'
 
 class SystemCalc:
 	def __init__(self):
