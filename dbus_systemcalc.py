0bb833274ade3e6b9be1d22b49829e96d02a8bb9
Ernst de Vries <edevries@victronenergy.com>
2015-06-02 16:13:06 +0200

    Bumped version to v1.07

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -21,7 +21,7 @@ from dbusmonitor import DbusMonitor
 from settingsdevice import SettingsDevice
 from logger import setup_logging
 
-softwareVersion = '1.06'
+softwareVersion = '1.07'
 
 class SystemCalc:
 	def __init__(self):
