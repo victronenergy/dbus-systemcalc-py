93647789d4c4524d2398bfbb1fca9c7ff7460c52
Ernst de Vries <edevries@victronenergy.com>
2016-11-01 15:05:55 +0100

    Bumped version to v1.30

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -19,7 +19,7 @@ from logger import setup_logging
 import delegates
 from sc_utils import safeadd as _safeadd, safemax as _safemax
 
-softwareVersion = '1.29'
+softwareVersion = '1.30'
 
 class SystemCalc:
 	def __init__(self):
