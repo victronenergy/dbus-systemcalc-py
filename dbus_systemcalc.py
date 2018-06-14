c14f02430ec959db74e888064f2c2cb5827a3b5d
Izak Burger <isburger@gmail.com>
2018-06-14 17:19:48 +0200

    bump version to 1.61

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -19,7 +19,7 @@ from logger import setup_logging
 import delegates
 from sc_utils import safeadd as _safeadd, safemax as _safemax
 
-softwareVersion = '1.60'
+softwareVersion = '1.61'
 
 class SystemCalc:
 	def __init__(self):
