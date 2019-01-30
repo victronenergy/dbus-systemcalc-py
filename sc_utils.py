89ca6cb67e44cab7d2c7f55b4d9fdd460f2e77e1
Izak Burger <isburger@gmail.com>
2019-01-30 16:05:04 +0200

    Add smart_dict helper object.
    This is a dictionary object that exposes its keys as
    attributes. Makes for more beautiful code.

--- sc_utils.py
+++ sc_utils.py
@@ -1,4 +1,5 @@
 from functools import update_wrapper
+from collections import Mapping
 
 VictronServicePrefix = 'com.victronenergy'
 
@@ -74,3 +75,16 @@ class reify(object):
 		val = self.wrapped(inst)
 		setattr(inst, self.wrapped.__name__, val)
 		return val
+
+class smart_dict(dict):
+	# Dictionary that can be accessed via attributes.
+	def __getattr__(self, k):
+		try:
+			v = self[k]
+			if isinstance(v, Mapping):
+				return self.__class__(v)
+			return v
+		except KeyError:
+			raise AttributeError(k)
+	def __setattr__(self, k, v):
+		self[k] = v
