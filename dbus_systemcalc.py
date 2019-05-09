0f92f9596a035c9f35105e305e283c377ba93226
Izak Burger <isburger@gmail.com>
2019-05-09 13:43:00 +0200

    v1.92

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -20,7 +20,7 @@ from logger import setup_logging
 import delegates
 from sc_utils import safeadd as _safeadd, safemax as _safemax
 
-softwareVersion = '1.91'
+softwareVersion = '1.92'
 
 class SystemCalc:
 	def __init__(self):
