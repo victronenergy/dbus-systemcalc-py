6606e01c6b78756a380cf3a96cc8a4c14af4b077
mpvader <mvader@victronenergy.com>
2019-07-03 22:59:57 +0200

    v2.07

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -20,7 +20,7 @@ from logger import setup_logging
 import delegates
 from sc_utils import safeadd as _safeadd, safemax as _safemax
 
-softwareVersion = '2.06'
+softwareVersion = '2.07'
 
 class SystemCalc:
 	def __init__(self):
