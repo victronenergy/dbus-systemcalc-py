1e231ba0a87d2c37f24ed1ca57ebfad317ed6bb6
Izak Burger <isburger@gmail.com>
2021-05-27 14:41:25 +0200

    fix hubType indication
    When turning a Multi off, the default machinery considers it disconnected. There is however
    enough data left on the service to determine whether it is an ESS system. Therefore we override
    the default implementation and check the service directly to determine the hub type.
    
    This fixes:
     - System type is no longer indicated as Hub-1 when you turn off the Multi.
     - The overview does not change when the Multi is turned off
     - The critical loads block on the GUI remains visible
    
    https://github.com/victronenergy/venus/issues/806

--- delegates/hubtype.py
+++ delegates/hubtype.py
@@ -8,12 +8,19 @@ class HubTypeSelect(SystemCalcDelegate):
 	def get_output(self):
 		return [('/Hub', {'gettext': '%s'}), ('/SystemType', {'gettext': '%s'})]
 
+	def get_multi(self):
+		services = sorted(self._dbusmonitor.get_service_list('com.victronenergy.vebus').items(),
+			key=lambda x: x[1])
+		if services:
+			return services[0][0]
+		return None
+
 	def update_values(self, newvalues):
 		# The code below should be executed after PV inverter data has been updated, because we need the
 		# PV inverter total power to update the consumption.
 		hub = None
 		system_type = None
-		vebus_path = newvalues.get('/VebusService')
+		vebus_path = self.get_multi()
 		hub4_assistant_id = self._dbusmonitor.get_value(vebus_path, '/Hub4/AssistantId')
 		if hub4_assistant_id is not None:
 			hub = 4
