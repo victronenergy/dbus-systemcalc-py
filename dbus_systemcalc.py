8ab3e5976b9d24616a7a6a25678df2af416f0ee8
Izak Burger <isburger@gmail.com>
2023-03-15 15:40:09 +0200

    v2.115

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -21,7 +21,7 @@ from logger import setup_logging
 import delegates
 from sc_utils import safeadd as _safeadd, safemax as _safemax
 
-softwareVersion = '2.114'
+softwareVersion = '2.115'
 
 class SystemCalc:
 	STATE_IDLE = 0
