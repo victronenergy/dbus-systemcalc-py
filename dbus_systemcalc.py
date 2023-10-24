e320def46efafa9a9855102a6fd2dfbc89976990
Izak Burger <iburger@victronenergy.com>
2023-10-24 14:59:17 +0200

    v2.136

--- dbus_systemcalc.py
+++ dbus_systemcalc.py
@@ -21,7 +21,7 @@ from logger import setup_logging
 import delegates
 from sc_utils import safeadd as _safeadd, safemax as _safemax
 
-softwareVersion = '2.135'
+softwareVersion = '2.136'
 
 class SystemCalc:
 	STATE_IDLE = 0
