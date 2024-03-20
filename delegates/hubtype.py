552f17101b2c733116accca6af135c31e0da0bbe
Izak Burger <iburger@victronenergy.com>
2024-03-20 17:45:23 +0200

    hubtype: Don't default to Hub-1
    If there is no Multi, many systems will show Hub-1 as long as
    there is PV power present. That is not correct. Now we show
    invalid instead.
    
    In future we'll properly deal with the Multi-RS's precise hub
    type, or some other method of showing ESS capacility rather than
    a text string.

--- delegates/hubtype.py
+++ delegates/hubtype.py
@@ -21,20 +21,21 @@ class HubTypeSelect(SystemCalcDelegate):
 		hub = None
 		system_type = None
 		vebus_path = self.get_multi()
-		hub4_assistant_id = self._dbusmonitor.get_value(vebus_path, '/Hub4/AssistantId')
-		if hub4_assistant_id is not None:
-			hub = 4
-			system_type = 'ESS' if hub4_assistant_id == 5 else 'Hub-4'
-		elif self._dbusmonitor.get_value(vebus_path, '/Hub/ChargeVoltage') is not None or \
-			newvalues.get('/Dc/Pv/Power') is not None:
-			hub = 1
-			system_type = 'Hub-1'
-		elif newvalues.get('/Ac/PvOnOutput/NumberOfPhases') is not None:
-			hub = 2
-			system_type = 'Hub-2'
-		elif newvalues.get('/Ac/PvOnGrid/NumberOfPhases') is not None or \
-			newvalues.get('/Ac/PvOnGenset/NumberOfPhases') is not None:
-			hub = 3
-			system_type = 'Hub-3'
+		if vebus_path is not None:
+			hub4_assistant_id = self._dbusmonitor.get_value(vebus_path, '/Hub4/AssistantId')
+			if hub4_assistant_id is not None:
+				hub = 4
+				system_type = 'ESS' if hub4_assistant_id == 5 else 'Hub-4'
+			elif self._dbusmonitor.get_value(vebus_path, '/Hub/ChargeVoltage') is not None or \
+				newvalues.get('/Dc/Pv/Power') is not None:
+				hub = 1
+				system_type = 'Hub-1'
+			elif newvalues.get('/Ac/PvOnOutput/NumberOfPhases') is not None:
+				hub = 2
+				system_type = 'Hub-2'
+			elif newvalues.get('/Ac/PvOnGrid/NumberOfPhases') is not None or \
+				newvalues.get('/Ac/PvOnGenset/NumberOfPhases') is not None:
+				hub = 3
+				system_type = 'Hub-3'
 		newvalues['/Hub'] = hub
 		newvalues['/SystemType'] = system_type
