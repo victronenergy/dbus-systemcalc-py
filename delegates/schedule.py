b18d58f971c362ab328cd6e9a8f950cd4357a632
Izak Burger <iburger@victronenergy.com>
2024-12-02 17:20:42 +0200

    refactor scheduled charge delegate
    This allows adding Multi-RS functionality more easily. It also
    adds /Control/ScheduledChargeStatus that explains if there is
    anything preventing it from working.

--- delegates/schedule.py
+++ delegates/schedule.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from enum import IntEnum
 import logging
 from gi.repository import GLib
 from datetime import datetime, timedelta, time, date
@@ -103,6 +104,80 @@ class ScheduledChargeWindow(ScheduledWindow):
 		return "Start charge: {}, Stop: {}, Soc: {}".format(
 			self.start, self.stop, self.soc)
 
+class EssDevice(object):
+	def __init__(self, delegate, monitor, service):
+		self.delegate = delegate
+		self.monitor = monitor
+		self.service = service
+
+	def check_conditions(self):
+		return 0
+
+	def _forcecharge(self):
+		raise NotImplementedError("forcecharge")
+
+	def _set_forcecharge(self, v):
+		raise NotImplementedError("forcecharge")
+
+	def _maxdischargepower(self):
+		raise NotImplementedError("maxdischargepower")
+
+	def _set_maxdischargepower(self, v):
+		raise NotImplementedError("maxdischargepower")
+
+	@property
+	def forcecharge(self):
+		return self._forcecharge()
+
+	@forcecharge.setter
+	def forcecharge(self, v):
+		return self._set_forcecharge(v)
+
+	@property
+	def maxdischargepower(self):
+		return self._maxdischargepower()
+
+	@maxdischargepower.setter
+	def maxdischargepower(self, v):
+		return self._set_maxdischargepower(v)
+
+class VebusDevice(EssDevice):
+	def check_conditions(self):
+		if not Dvcc.instance.has_ess_assistant:
+			return Reasons.NO_ESS
+		if BatteryLife.instance.state == BatteryLifeState.KeepCharged:
+			return Reasons.ESS_MODE
+		return 0
+
+	def _forcecharge(self):
+		return self.monitor.get_value(HUB4_SERVICE, '/Overrides/ForceCharge')
+
+	def _set_forcecharge(self, v):
+		return self.monitor.set_value_async(HUB4_SERVICE,
+			'/Overrides/ForceCharge', 1 if v else 0)
+
+	def _maxdischargepower(self):
+		return self.monitor.get_value(HUB4_SERVICE,
+			'/Overrides/MaxDischargePower')
+
+	def _set_maxdischargepower(self, v):
+		return self.monitor.set_value_async(HUB4_SERVICE,
+			'/Overrides/MaxDischargePower', -1 if v is None else v)
+
+class Reasons(IntEnum):
+	OK = 0
+	NO_ESS = 1
+	NO_SOC = 2
+	ESS_MODE = 3
+	BLOCKED = 4
+
+	@classmethod
+	def get_text(cls, v):
+		try:
+			return {v: k for k, v in cls.__members__.items()}[v]
+		except KeyError:
+			return '--'
+
 class ScheduledCharging(SystemCalcDelegate, ChargeControl):
 	""" Let the system do other things based on time schedule. """
 	control_priority = 20
@@ -113,13 +188,20 @@ class ScheduledCharging(SystemCalcDelegate, ChargeControl):
 		self.pvpower = 0
 		self.active = False
 		self.hysteresis = True
+		self.devices = []
 		self._timer = GLib.timeout_add(5000, exit_on_error, self._on_timer)
 
 	def set_sources(self, dbusmonitor, settings, dbusservice):
 		super(ScheduledCharging, self).set_sources(dbusmonitor, settings, dbusservice)
 		self._dbusservice.add_path('/Control/ScheduledCharge', value=0)
+		self._dbusservice.add_path('/Control/ScheduledChargeStatus', value=None,
+			gettextcallback=lambda p, v: Reasons.get_text(v))
 		self._dbusservice.add_path('/Control/ScheduledSoc', value=None,
 			gettextcallback=lambda p, v: '{}%'.format(v))
+
+		# Assume a VE.Bus device is present. If not, check_conditions() will
+		# return non-zero.
+		self.devices.append(VebusDevice(self, dbusmonitor, None))
 
 	def get_input(self):
 		return [
@@ -171,37 +253,26 @@ class ScheduledCharging(SystemCalcDelegate, ChargeControl):
 		discharges = (self._settings['schedule_discharge_{}'.format(i)] for i in range(NUM_SCHEDULES))
 		return self._charge_windows(today, days, starttimes, durations, stopsocs, discharges)
 
-	@property
-	def forcecharge(self):
-		return self._dbusmonitor.get_value(HUB4_SERVICE, '/Overrides/ForceCharge')
-
-	@forcecharge.setter
-	def forcecharge(self, v):
-		return self._dbusmonitor.set_value_async(HUB4_SERVICE,
-			'/Overrides/ForceCharge', 1 if v else 0)
-
-	@property
-	def maxdischargepower(self):
-		return self._dbusmonitor.get_value(HUB4_SERVICE, '/Overrides/MaxDischargePower')
-
-	@maxdischargepower.setter
-	def maxdischargepower(self, v):
-		return self._dbusmonitor.set_value_async(HUB4_SERVICE, '/Overrides/MaxDischargePower', v)
-
 	def _on_timer(self):
 		if self.soc is None:
+			self._dbusservice['/Control/ScheduledChargeStatus'] = Reasons.NO_SOC
+			self._dbusservice['/Control/ScheduledCharge'] = 0
+			self._dbusservice['/Control/ScheduledSoc'] = None
 			return True
 
-		if not Dvcc.instance.has_ess_assistant:
-			return True
-
-		if BatteryLife.instance.state == BatteryLifeState.KeepCharged:
+		condition = Reasons.NO_ESS
+		for device in self.devices:
+			if (condition := device.check_conditions()) == 0:
+				break
+		else:
+			self._dbusservice['/Control/ScheduledChargeStatus'] = condition
 			self._dbusservice['/Control/ScheduledCharge'] = 0
 			self._dbusservice['/Control/ScheduledSoc'] = None
 			return True
 
 		# Another delegate controls charging
 		if not self.can_acquire_control:
+			self._dbusservice['/Control/ScheduledChargeStatus'] = Reasons.BLOCKED
 			self._dbusservice['/Control/ScheduledCharge'] = 0
 			self._dbusservice['/Control/ScheduledSoc'] = None
 			return True
@@ -212,17 +283,17 @@ class ScheduledCharging(SystemCalcDelegate, ChargeControl):
 		for w in self.charge_windows(today):
 			if now in w:
 				if w.soc_reached(self.soc):
-					self.forcecharge = False
+					device.forcecharge = False
 				elif self.hysteresis and w.soc_reached(self.soc + 5):
 					# If we are within 5%, keep it the same, but write it to
 					# avoid a timeout.
-					self.forcecharge = self.forcecharge
+					device.forcecharge = device.forcecharge
 				else:
 					# SoC not reached yet
 					# Note: soc_reached always returns False for a target of
 					# 100%, so this is the only branch that is ever excuted
 					# in those cases.
-					self.forcecharge = True
+					device.forcecharge = True
 
 				# Signal that scheduled charging is active
 				self.acquire_control() # Block out other controllers
@@ -237,8 +308,8 @@ class ScheduledCharging(SystemCalcDelegate, ChargeControl):
 				# setting a discharge limit if we're currently charging, in
 				# other words, if we're below the target soc, or if the target
 				# soc is 100%.
-				if self.forcecharge:
-					self.maxdischargepower = -1
+				if device.forcecharge:
+					device.maxdischargepower = None
 					break # from the for loop, skip the else clause below.
 
 				# If we are here, it means the battery has reached the target
@@ -256,19 +327,20 @@ class ScheduledCharging(SystemCalcDelegate, ChargeControl):
 				# The ESS MinSoc is still obeyed and takes precedence.
 				delta = max(0, self.soc - w.soc)
 				if delta > 1 and w.allow_discharge:
-					self.maxdischargepower = -1
+					device.maxdischargepower = None
 				else:
 					scale = 0.8 + min(delta, 1)*0.15
-					self.maxdischargepower = max(1, round(self.pvpower*scale))
+					device.maxdischargepower = max(1, round(self.pvpower*scale))
 				break
 		else:
-			self.forcecharge = False
-			self.maxdischargepower = -1
+			device.forcecharge = False
+			device.maxdischargepower = None
 			self.active = False
 			self.release_control()
 			self._dbusservice['/Control/ScheduledSoc'] = None
 
 		self._dbusservice['/Control/ScheduledCharge'] = int(self.active)
+		self._dbusservice['/Control/ScheduledChargeStatus'] = Reasons.OK
 		self.hysteresis = True
 		return True
 
