60b3eba1362acd1923617424b9a239ac97d4eba9
ReinvdZee <rvanderzee@victronenergy.com>
2025-01-17 16:27:06 +0100

    Soluna battery settings

--- delegates/batterysettings.py
+++ delegates/batterysettings.py
@@ -30,6 +30,7 @@ BATTERY_LEOCH = 0xB01D
 BATTERY_LBSA = 0xB01E
 BATTERY_SUNWODA = 0xB01F
 BATTERY_BATRIUM_D = 0xB038
+BATTERY_SOLUNA = 0xB02F
 
 class BatterySettings(SystemCalcDelegate):
 	""" Manages battery settings for known batteries. At present
@@ -60,7 +61,7 @@ class BatterySettings(SystemCalcDelegate):
 			self._settings['bol'] = 3 # Forced on
 		elif pid in (BATTERY_LYNX_SMART_BMS_500, BATTERY_LYNX_SMART_BMS_1000,
 				BATTERY_LYNX_SMART_BMS_500_NG, BATTERY_LYNX_SMART_BMS_1000_NG,
-				BATTERY_PARALLEL_BMS, BATTERY_METERBOOST, BATTERY_BATRIUM_D):
+				BATTERY_PARALLEL_BMS, BATTERY_METERBOOST, BATTERY_BATRIUM_D, BATTERY_SOLUNA):
 			self._settings['vsense'] = 3 # Forced on
 			self._settings['tsense'] = 2 # Forced Off
 			self._settings['bol'] = 3 # Forced on
