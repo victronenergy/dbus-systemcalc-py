68bf697bcfddf08708291682bdb67a50bb322c8d
Daniel Manner <daniel.manner@equinox-solutions.de>
2026-04-30 17:45:34 +0200

    PvOpMon: Added monitoring for acsystem0

--- delegates/pvopmon.py
+++ delegates/pvopmon.py
@@ -11,13 +11,13 @@ class PvOpMon(SystemCalcDelegate):
 		super(PvOpMon, self).__init__()
 		self.systemcalc = sc
 		self._pv_disabled = ExpiringValue(20, False)
+		self._acsystem0 = None
 
 	def set_sources(self, dbusmonitor, settings, dbusservice):
 		super(PvOpMon, self).set_sources(dbusmonitor, settings, dbusservice)
 
 		self._dbusservice.add_path('/Pv/Disable', value=0, writeable=True,
 			onsetcallback=self._dbus_path_set)
-
 
 	def get_input(self):
 		return [
@@ -35,7 +35,8 @@ class PvOpMon(SystemCalcDelegate):
 		#DVCC will read the pv_disabled property. dbus-shelly can provide shelly based inverters it can turn off.
 		self._pv_disabled.set(disabled)
 		self._dbusmonitor.set_value_async("com.victronenergy.hub4", "/Pv/Disable", int(disabled))
-		self._dbusmonitor.set_value_async("com.victronenergy.acsystem", "/Pv/Disable", int(disabled))
+		if self._acsystem0:
+			self._dbusmonitor.set_value_async(self._acsystem0, "/Pv/Disable", int(disabled))
 		self._dbusmonitor.set_value_async("com.victronenergy.shelly", "/Pv/Disable", int(disabled))
 
 		return True
@@ -44,6 +45,7 @@ class PvOpMon(SystemCalcDelegate):
 	def pv_disabled(self) -> bool:
 		'''
 			Flag, if dess requests to disable PV. Defaults to False. Will timeout after 20 reads.
+			DVCC will read continiously and timeout the value when no longer set.
 		'''
 		disabled = self._pv_disabled.get() or False
 		#logger.info("Retrieved pv disabled as: {} ({} reads left)".format(disabled, self._pv_disabled._ttl))
@@ -54,7 +56,8 @@ class PvOpMon(SystemCalcDelegate):
 
 			#also restore operation for hub4 and multi rs
 			self._dbusmonitor.set_value_async("com.victronenergy.hub4", "/Pv/Disable", 0)
-			self._dbusmonitor.set_value_async("com.victronenergy.acsystem", "/Pv/Disable", 0)
+			if self._acsystem0:
+				self._dbusmonitor.set_value_async(self._acsystem0, "/Pv/Disable", 0)
 			self._dbusmonitor.set_value_async("com.victronenergy.shelly", "/Pv/Disable", 0)
 			self._dbusservice["/Pv/Disable"] = 0
 
@@ -65,3 +68,15 @@ class PvOpMon(SystemCalcDelegate):
 
 	def update_values(self, newvalues):
 		pass
+
+	def device_added(self, service, instance, *args, **kwargs):
+		service_type = service.split('.')[2]
+		if service_type == 'acsystem':
+			if self._dbusmonitor.get_value(service, '/DeviceInstance') == 0:
+				self._acsystem0 = service
+
+	def device_removed(self, service, instance, *args, **kwargs):
+		service_type = service.split('.')[2]
+		if service_type == 'acsystem':
+			if service == self._acsystem0:
+				self._acsystem0 = None
